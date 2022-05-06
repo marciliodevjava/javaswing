4a0dce8d9c3113093d1b94dab202a788c1b6d6ff
m-arc <m-arc@192.168.1.8>
2022-05-06 16:18:43 -0300

    Pradroes Observer Class EventoChegadaAniversariante

---
+++ src/padroes/oberver/EventoChegadaAniversariante.java
@@ -0,0 +1,18 @@
+package padroes.oberver;
+
+import java.util.Date;
+
+public class EventoChegadaAniversariante {
+
+	private final Date momento;
+
+	public EventoChegadaAniversariante(Date momento) {
+		super();
+		this.momento = momento;
+	}
+
+	public Date getMomento() {
+		return momento;
+	}
+	
+}
