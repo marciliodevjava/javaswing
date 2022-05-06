1b2f02425d47c1d7dbb13e8520ac2fd8bc5da4d6
m-arc <m-arc@192.168.1.8>
2022-05-06 16:32:28 -0300

    Padrões Observer Class Porteiro

--- src/padroes/oberver/Porteiro.java
+++ src/padroes/oberver/Porteiro.java
@@ -1,7 +1,9 @@
 package padroes.oberver;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 public class Porteiro {
 
@@ -9,5 +11,24 @@ public class Porteiro {
 
 	public void registrarObservador(ObservadorChegadaAniversarianteObservador observador) {
 		oberservadores.add(observador);
+	}
+	
+	public void mnitorar() {
+		Scanner entrada = new Scanner(System.in);
+		String valor = "";
+		while(!"sair".equalsIgnoreCase(valor)) {
+			System.out.println("Aniversáriante chegou?");
+			valor = entrada.nextLine();
+			if("sim".equalsIgnoreCase(valor)) {
+				//Criar o evento
+				EventoChegadaAniversariante evento = new EventoChegadaAniversariante(new Date());
+				//notificar os observadores!
+				oberservadores.stream().forEach(o -> o.chegou(evento));
+			} else {
+				System.out.println("Alarme falso!");
+			}
+		}
+		
+		entrada.close();
 	}
 }
